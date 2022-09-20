b2e84e03bf0e1bafd921055018759c2579144d75
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-09-20 11:53:49 +0900

    0920

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Main from './pages/Main';
+import Sub01 from './pages/Sub01';
+import Sub02 from './pages/Sub02';
+import NotFound from './pages/NotFound';
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/sub01' element={<Sub01 />} />
+        <Route path='/sub02' element={<Sub02 />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <Footer />
 
-    </div>
-  );
+
+
+    </>
+
+  )
 }
 
-export default App;
+export default App
